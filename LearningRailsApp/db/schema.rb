fe1d93161efbd3ad294e4d729402a32accf2c691
Alexandra Fren <alexandra.fren@gmail.com>
2018-08-25 18:56:34 +0000

    add a resource generator

--- LearningRailsApp/db/schema.rb
+++ LearningRailsApp/db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_25_161113) do
+ActiveRecord::Schema.define(version: 2018_08_25_183337) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_img"
+    t.text "thumb_img"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
