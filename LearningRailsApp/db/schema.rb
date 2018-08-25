07b7e15f64893a0203302b827fd2484343b663d9
Alexandra Fren <alexandra.fren@gmail.com>
2018-08-25 16:27:45 +0000

    model generator skills

--- LearningRailsApp/db/schema.rb
+++ LearningRailsApp/db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_25_144658) do
+ActiveRecord::Schema.define(version: 2018_08_25_161113) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "percent"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
